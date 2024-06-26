a7dcd661ee39aac6fcacbc72031e895e506188ee
yanncauchepin <cauchepin.yann@gmail.com>
2024-06-26 20:10:06 +0200

    init

--- main.py
+++ main.py
@@ -14,10 +14,15 @@ def loto_gnb():
     one_hot_ball_df = pd.read_csv('data/all_one_hot_ball_loto.csv', index_col=0)
     one_hot_star_df = pd.read_csv('data/all_one_hot_star_loto.csv', index_col=0)
     
-    predictions_ball = one_hot_gnb(one_hot_ball_df, 5, 49)
-    predictions_star = one_hot_gnb(one_hot_star_df, 1, 10)
+    pred_next_ball, next_ball = one_hot_gnb(one_hot_ball_df, 5, 49)
+    pred_next_star, next_star = one_hot_gnb(one_hot_star_df, 1, 10)
     
-    return {'predictions_ball': predictions_ball, 'predictions_star': predictions_star}
+    return {
+        'pred_next_ball': pred_next_ball,
+        'next_ball': next_ball,
+        'pred_next_star': pred_next_star,
+        'next_star': next_star
+        }
 
 
 def loto_lstm():
@@ -27,19 +32,45 @@ def loto_lstm():
     one_hot_ball_df = pd.read_csv('data/all_one_hot_ball_loto.csv', index_col=0)
     one_hot_star_df = pd.read_csv('data/all_one_hot_star_loto.csv', index_col=0)
 
-    predictions_ball = one_hot_lstm(one_hot_ball_df, 5)
-    predictions_star = one_hot_lstm(one_hot_star_df, 1)
+    _, pred_next_ball, next_ball = one_hot_lstm(one_hot_ball_df, 5)
+    _, pred_next_star, next_star = one_hot_lstm(one_hot_star_df, 1)
 
-    return {'predictions_ball': predictions_ball, 'predictions_star': predictions_star}
-
+    return {
+        'pred_next_ball': pred_next_ball,
+        'next_ball': next_ball,
+        'pred_next_star': pred_next_star,
+        'next_star': next_star
+        }
 
 def loto_random():
     
     predictions_ball = random_draw(49, 5)
     predictions_star = random_draw(10, 1)
     
-    return {'predictions_ball': predictions_ball, 'predictions_star': predictions_star}
+    return {
+        'next_ball': next_ball,
+        'next_star': next_star
+        }
 
+def loto_ensemble():
+    
+    lstm = loto_lstm()
+    gnb = loto_gnb()
+    
+    sum_pred_ball = gnb["pred_next_ball"].add(
+        lstm["pred_next_ball"])
+    sum_pred_ball = sum_pred_ball.sort_values(by=0, ascending=False)
+    print(f"Ensemble ball:\n{sum_pred_ball}")
+    
+    sum_pred_star = gnb["pred_next_star"].add(
+        lstm["pred_next_star"])
+    sum_pred_star = sum_pred_star.sort_values(by=0, ascending=False)
+    print(f"Ensemble star:\n{sum_pred_star}")
+    
+    return {
+        'sum_pred_ball': sum_pred_ball,
+        'sum_pred_star': sum_pred_star
+    }
 
 def euromillions_gnb():
     
@@ -48,11 +79,16 @@ def euromillions_gnb():
     one_hot_ball_df = pd.read_csv('data/all_one_hot_ball_euromillions.csv', index_col=0)
     one_hot_star_df = pd.read_csv('data/all_one_hot_star_euromillions.csv', index_col=0)
     
-    predictions_ball = one_hot_gnb(one_hot_ball_df, 5, 50)
-    predictions_star = one_hot_gnb(one_hot_star_df, 2, 12)
+    pred_next_ball, next_ball = one_hot_gnb(one_hot_ball_df, 5, 50)
+    pred_next_star, next_star = one_hot_gnb(one_hot_star_df, 2, 12)
     
-    return {'predictions_ball': predictions_ball, 'predictions_star': predictions_star}
-
+    return {
+        'pred_next_ball': pred_next_ball,
+        'next_ball': next_ball,
+        'pred_next_star': pred_next_star,
+        'next_star': next_star
+        }
+    
 def euromillions_lstm():
     
     euromillions_data()
@@ -60,58 +96,66 @@ def euromillions_lstm():
     one_hot_ball_df = pd.read_csv('data/all_one_hot_ball_euromillions.csv', index_col=0)
     one_hot_star_df = pd.read_csv('data/all_one_hot_star_euromillions.csv', index_col=0)
 
-    predictions_ball = one_hot_lstm(one_hot_ball_df, 5)
-    predictions_star = one_hot_lstm(one_hot_star_df, 2)
+    _, pred_next_ball, next_ball = one_hot_lstm(one_hot_ball_df, 5)
+    _, pred_next_star, next_star = one_hot_lstm(one_hot_star_df, 2)
 
-    return {'predictions_ball': predictions_ball, 'predictions_star': predictions_star}
-
+    return {
+        'pred_next_ball': pred_next_ball,
+        'next_ball': next_ball,
+        'pred_next_star': pred_next_star,
+        'next_star': next_star
+        }
 
 def euromillions_random():
     
     predictions_ball = random_draw(50, 5)
     predictions_star = random_draw(12, 2)
     
-    return {'predictions_ball': predictions_ball, 'predictions_star': predictions_star}
+    return {
+        'next_ball': next_ball,
+        'next_star': next_star
+        }
 
+def euromillions_ensemble():
+    
+    lstm = euromillions_lstm()
+    gnb = euromillions_gnb()
+    
+    sum_pred_ball = gnb["pred_next_ball"].add(
+        lstm["pred_next_ball"])
+    sum_pred_ball = sum_pred_ball.sort_values(by=0, ascending=False)
+    print(f"Ensemble ball:\n{sum_pred_ball}")
+    
+    sum_pred_star = gnb["pred_next_star"].add(
+        lstm["pred_next_star"])
+    sum_pred_star = sum_pred_star.sort_values(by=0, ascending=False)
+    print(f"Ensemble star:\n{sum_pred_star}")
+    
+    return {
+        'sum_pred_ball': sum_pred_ball,
+        'sum_pred_star': sum_pred_star
+    }
 
 def eurodreams_random():
     
     predictions_ball = random_draw(40, 6)
     predictions_star = random_draw(5, 1)
     
-    return {'predictions_ball': predictions_ball, 'predictions_star': predictions_star}
-
+    return {
+        'next_ball': next_ball,
+        'next_star': next_star
+        }
 
 if __name__=='__main__':
     
-    euromillions_gnb = euromillions_gnb()
-    euromillions_lstm = euromillions_lstm()
-    euromillions_random = euromillions_random()
-    # eurodreams_random = eurodreams_random()
+    # euromillions_gnb = euromillions_gnb()
+    # euromillions_lstm = euromillions_lstm()
+    # euromillions_random = euromillions_random()
+    # euromillions_ensemble = euromillions_ensemble()
+    
     # loto_gnb = loto_gnb()
     # loto_lstm = loto_lstm()
     # loto_random = loto_random()
+    # loto_ensemble = loto_ensemble()
     
-    
-# =============================================================================
-#     EXPERIMENTAL EUROMILLIONS
-    sum_prob_ball = euromillions_gnb["predictions_ball"][0].add(
-        euromillions_lstm["predictions_ball"][1])
-    sum_prob_ball = sum_prob_ball.sort_values(by=0, ascending=False)
-    sum_prob_star = euromillions_gnb["predictions_star"][0].add(
-        euromillions_lstm["predictions_star"][1])
-    sum_prob_star = sum_prob_star.sort_values(by=0, ascending=False)
-
-# =============================================================================
-
-# =============================================================================
-#     EXPERIMENTAL LOTO
-    # sum_prob_ball = loto_gnb["predictions_ball"][0].add(
-    #     loto_lstm["predictions_ball"][1])
-    # sum_prob_ball = sum_prob_ball.sort_values(by=0, ascending=False)
-    # sum_prob_star = loto_gnb["predictions_star"][0].add(
-    #     loto_lstm["predictions_star"][1])
-    # sum_prob_star = sum_prob_star.sort_values(by=0, ascending=False)
-# =============================================================================
-  
-    
+    # eurodreams_random = eurodreams_random()
--- model/one_hot_gnb.py
+++ model/one_hot_gnb.py
@@ -11,7 +11,7 @@ def meta_modeling(df, size, numbers):
     
     # Iterate over each target variable (assuming columns represent target variables)
     for i in range(numbers):
-        # import pdb; pdb.set_trace()
+
         # Extract the target variable
         X = np.arange(len(df)).reshape(-1, 1).astype(np.int32)
         y = df.iloc[:, i].values.ravel()
@@ -29,7 +29,7 @@ def meta_modeling(df, size, numbers):
         y_probs_list.append(y_probs)
     
     pred = list()
-    # import pdb; pdb.set_trace()
+
     for i in range(numbers):
         pred.append(y_probs_list[i][0][0])
     next_y = np.argpartition(pred, -size)[-size:]
@@ -40,82 +40,3 @@ def meta_modeling(df, size, numbers):
     print(f'Next values: {next_y}')
     
     return pred, next_y
-
-if __name__ == '__main__':
-    
-# =============================================================================
-# from sklearn.naive_bayes import GaussianNB
-# from sklearn.metrics import accuracy_score
-# 
-# n_variables = 5
-# n_times = 100
-# indices = np.random.randint(0, n_variables, size=n_times)
-# one_hot_time_series = np.eye(n_variables)[indices]
-# 
-# X = np.arange(n_times).reshape(-1, 1).astype(np.float64)  # GNB expects float64
-# y = one_hot_time_series  # Use class labels directly
-# 
-# # Split data into train and test sets
-# train_size = int(0.8 * n_times)
-# X_train, X_test = X[:train_size], X[train_size:]
-# y_train, y_test = y[:train_size], y[train_size:]
-# 
-# # Initialize Gaussian Naive Bayes models for each output variable
-# models = []
-# for i in range(n_variables):
-#     gnb = GaussianNB(priors=[1/n_variables]*n_variables)
-#     gnb.fit(X_train, y_train[:, i])
-#     models.append(gnb)
-# 
-# # Predict probabilities for each output variable
-# y_probs = []
-# for model in models:
-#     probs = model.predict_proba(X_test)[:, 1]  # Get probabilities of the positive class
-#     y_probs.append(probs)
-# 
-# # Convert list of probabilities to numpy array
-# y_probs = np.array(y_probs).T
-# 
-# # Evaluate performance (Optional)
-# # You can calculate accuracy if you have ground truth labels
-# y_pred = np.argmax(y_probs, axis=1)  # Convert probabilities to class labels
-# accuracy = accuracy_score(np.argmax(y_test, axis=1), y_pred)
-# print("Accuracy:", accuracy)
-# 
-# # Print predicted probabilities
-# print("Predicted Probabilities:", y_probs)
-# =============================================================================
-
-    from sklearn.naive_bayes import GaussianNB
-    from sklearn.metrics import accuracy_score
-    import numpy as np
-    
-    n_classes = 50
-    n_times = 1000
-    # Generate a unique variable of integers from 1 to 50
-    labels = np.random.randint(1, n_classes + 1, size=n_times)
-    
-    X = np.arange(n_times).reshape(-1, 1).astype(np.float64)  # GNB expects float64
-    y = labels  # Use integer labels directly
-    
-    # Split data into train and test sets
-    train_size = int(0.8 * n_times)
-    X_train, X_test = X[:train_size], X[train_size:]
-    y_train, y_test = y[:train_size], y[train_size:]
-    
-    # Initialize Gaussian Naive Bayes model
-    gnb = GaussianNB(priors=[1/n_classes]*n_classes)
-    
-    # Fit the model
-    gnb.fit(X_train, y_train)
-    
-    # Predict
-    y_pred = gnb.predict(X_test)
-    
-    y_probs = gnb.predict_proba(X_test)
-    
-    # Evaluate performance
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Accuracy:", accuracy)
-    
-    print("Predicted Probabilities:", y_probs)
