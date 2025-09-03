7841ea723a50941ea3569a5bbdef9e7999f8ee2c
Yann Cauchepin <cauchepin.yann@gmail.com>
2025-09-03 17:34:30 +0200

    update

--- main.py
+++ main.py
@@ -10,29 +10,29 @@ from modules.random_draw import random_draw
 
 ROOT_PATH = Path(__file__).parent
 
-def loto_random():
+def loto_random(seed=None):
     
-    predictions_ball = random_draw(49, 5)
-    predictions_star = random_draw(10, 1)
-    print(f'===================\nBall predictions: {predictions_ball}\n===================\nStar predictions: {predictions_star}\n===================\n')
+    random_ball = random_draw(49, 5, seed)
+    random_star = random_draw(10, 1, seed)
+    print(f'===================\nBall predictions: {random_ball}\n===================\nStar predictions: {random_star}\n===================\n')
 
-    return predictions_ball, predictions_star
+    return random_ball, random_star
 
-def euromillions_random():
+def euromillions_random(seed=None):
     
-    predictions_ball = random_draw(50, 5)
-    predictions_star = random_draw(12, 2)
-    print(f'===================\nBall predictions: {predictions_ball}\n===================\nStar predictions: {predictions_star}\n===================\n')
+    random_ball = random_draw(50, 5, seed)
+    random_star = random_draw(12, 2, seed)
+    print(f'===================\nBall predictions: {random_ball}\n===================\nStar predictions: {random_star}\n===================\n')
 
-    return predictions_ball, predictions_star
+    return random_ball, random_star
 
-def eurodreams_random():
+def eurodreams_random(seed=None):
     
-    predictions_ball = random_draw(40, 6)
-    predictions_star = random_draw(5, 1)
-    print(f'===================\nBall predictions: {predictions_ball}\n===================\nStar predictions: {predictions_star}\n===================\n')
+    random_ball = random_draw(40, 6, seed)
+    random_star = random_draw(5, 1, seed)
+    print(f'===================\nBall predictions: {random_ball}\n===================\nStar predictions: {random_star}\n===================\n')
 
-    return predictions_ball, predictions_star
+    return random_ball, random_star
 
 def loto_one_hot_kf():
 
